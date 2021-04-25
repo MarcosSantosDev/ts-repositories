3021e09bc5903783d3e6a3689761b7a306fd8018
Marcos Santos <marcosadriano034@gmail.com>
2021-04-25 17:52:11 -0300

    fix(thunk): github redux thunk dispatching initial action

--- src/redux/thunks/gitHub.ts
+++ src/redux/thunks/gitHub.ts
@@ -3,12 +3,14 @@ import { getRepositoriesByUserName } from '../../services/GitHub/repositories';
 import { getUserByUserName } from '../../services/GitHub/user';
 
 export const getAllRepositories = (user: string) => ((dispatch: any) => {
+  dispatch(actions.getAllRepositories());
   getRepositoriesByUserName(user)
     .then((repositories: any) => dispatch(actions.getAllRepositoriesSuccess(repositories.data)))
     .catch((error: any) => dispatch(actions.getAllRepositoriesFail(error)));
 });
 
 export const getUser = (user: string) => ((dispatch: any) => {
+  dispatch(actions.getUser());
   getUserByUserName(user)
     .then((userData: any) => dispatch(actions.getUserSuccess(userData.data)))
     .catch((error: any) => dispatch(actions.getUserFail(error)));
