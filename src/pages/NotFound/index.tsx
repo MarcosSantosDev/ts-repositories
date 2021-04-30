bdf960c0d8e3564674a190829cb7bd244cdbd15e
Marcos Santos <marcosadriano034@gmail.com>
2021-04-30 01:04:12 -0300

    feat: find user by not found page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,12 +1,47 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { iconLoader } from '../../utils/iconLoader';
 
-import { Container } from './styled';
+import {
+  Container, FormWrapper, Input, Button,
+} from './styled';
 
-const NotFound: React.FC = () => (
-  <Container>
-    <div className="notFound-404">404</div>
-    <div className="notFound-message">Ops, page not found!</div>
-  </Container>
-);
+const NotFound: React.FC = () => {
+  const history = useHistory();
+
+  const handleOnSubmit = () => {
+    const elementInput: any = document.getElementById('userName');
+
+    if (elementInput && elementInput?.value) {
+      history.push(`/${elementInput?.value}`);
+    }
+  };
+
+  const handleOnSubmitByKeyUp = (event: any) => {
+    if (event.which === 13) {
+      handleOnSubmit();
+    }
+  };
+
+  return (
+    <Container>
+      <div className="notFound-404">404</div>
+      <div className="notFound-message">Ops, page not found!</div>
+      <FormWrapper>
+        <Input
+          type="text"
+          name="userName"
+          id="userName"
+          placeholder="Write here the name of the profile"
+          onKeyUp={handleOnSubmitByKeyUp}
+        />
+        <Button type="button" onClick={handleOnSubmit}>
+          Find profile
+          {iconLoader('github', 16)}
+        </Button>
+      </FormWrapper>
+    </Container>
+  );
+};
 
 export default NotFound;
--- src/pages/NotFound/styled.ts
+++ src/pages/NotFound/styled.ts
@@ -19,3 +19,60 @@ export const Container = styled.div`
     color: #f0f8ff;
   }
 `;
+
+export const FormWrapper = styled.main`
+  position: relative;
+  display: flex;
+  margin: 50px 0px;
+`;
+
+export const Input = styled.input`
+  width: 272px;
+  padding: 6px 12px;
+  min-height: 28px;
+  background: #0d1117;
+  color: #f0f6fc;
+  border: 1px solid #21262d;
+  border-radius: 5px;
+  font-size: 14px;
+  outline: none;
+  transition: .5s;
+  
+  &:focus {
+    width: 300px;
+  }
+
+  &::placeholder {
+    color: #c9d1d960;
+    font-size: 14px;
+  }
+`;
+
+export const Button = styled.button`
+    display: flex;
+    align-items: center;
+    flex-grow: 0;
+    flex-shrink: 1;
+    padding: 5px 16px;
+    margin: 0px 16px;
+    font-size: 14px;
+    font-weight: 500;
+    line-height: 20px;
+    cursor: pointer;
+    user-select: none;
+    border: 1px solid;
+    border-radius: 5px;
+    appearance: none;
+    color: #c9d1d9;
+    background-color: #21262d;
+    border-color: #30363d;
+    transition: .2s;
+
+    &:hover {
+      opacity: .5;
+    }
+
+    svg {
+      margin: 0 5px;
+    }
+`;
